29e976fad0918dff79f5cd670067a7427fd3fe8d
Ion Utale <utale.ion@gmail.com>
2019-11-01 21:45:17 +0100

    created clan component

---
+++ src/components/clan/clan.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import css from './clan.module.css'
+
+const Clan = (props) => {
+
+  const members = clanMembers => {
+    const arr = clanMembers // townHallLevel
+    .sort((a, b) => (a.townHallLevel > b.townHallLevel) ? -1 : ((a.townHallLevel < b.townHallLevel) ? 1 : 0))
+    .map(p => {
+      return (
+        <li className={css.MemberInfo}>
+          <p>{p.name} - {p.townHallLevel}</p>
+        </li>
+      )
+    })
+    console.log(arr);
+    return arr
+  }
+
+  return (
+    <div className={css.ClanContainer}>
+      <label>{props.clan.name}</label>
+      <img src={props.clan.badgeUrls.small} alt="clan badge"></img>
+      <ul className={css.MemberList}>
+        {members(props.clan.members)}
+      </ul>
+    </div>
+  );
+}
+
+export default Clan
