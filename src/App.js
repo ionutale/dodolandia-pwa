0ba5ca96d196f2148fa20213cb2a57382e347f9d
Ion Utale <utale.ion@gmail.com>
2019-11-05 19:13:36 +0100

    small improvements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import CWL from './containers/CWL/CWL'
+import packageJson from '../package.json';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        Clan War League
+        <p className="Version">
+        Made By With ❤️(_d0do_)  ©v{packageJson.version} 
+          </p>
       </header>
       <CWL></CWL>
     </div>
