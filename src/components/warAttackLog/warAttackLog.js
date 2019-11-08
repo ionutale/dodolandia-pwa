328ed01ce8638ef4697d5c96d4be223014a1f133
Ion Utale <utale.ion@gmail.com>
2019-11-08 19:04:36 +0100

    added  warAttackLog

---
+++ src/components/warAttackLog/warAttackLog.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ * Receive all data from clanLeagueWarTag
+ * TODO: maybe not a very good idea to send all data here
+ */
+const WarAttackLog = ({clanLeagueWarTag}) => {
+
+
+  return (
+    <div>WarAttackLog</div>
+  )
+}
+
+export default WarAttackLog
--- src/containers/CWL_war/CWL_war.js
+++ src/containers/CWL_war/CWL_war.js
@@ -3,6 +3,7 @@ import * as axios from '../../network/cwl-axios'
 import {clanLeagueWar} from '../../sampleData/clanLeagueWar'
 import css from './cwl_war.module.css'
 import WarMap from '../../components/warMap/warMap'
+import WarAttackLog from '../../components/warAttackLog/warAttackLog'
 
 const CWL_war = () => {
 const DODOLANDIA_TAG = 'YQP0P9PP'
@@ -161,6 +162,7 @@ const DODOLANDIA_TAG = 'YQP0P9PP'
 
   return (
     <div>
+      <WarAttackLog clanLeagueWar={clanLeagueWar}></WarAttackLog>
       <WarMap clanLeagueWar={clanLeagueWar}></WarMap>
       {/* {compareMapPos()} */}
       {/* {attacksLog(attackLog(clanLeagueWar), clanLeagueWar.clan)} */}
