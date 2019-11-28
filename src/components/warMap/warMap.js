32fd0b69ad47c35594b2d265b763324c26ff6066
Ion <ion.utale@icloud.com>
2019-11-28 16:19:00 +0100

    mnor fix

--- src/components/warMap/warMap.js
+++ src/components/warMap/warMap.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import css from './warMap.module.css'
-const WarMap = () => {
+const WarMap = (clanLeagueWar) => {
 
   const sortPlayers = members => members.sort((a, b) => (a.mapPosition < b.mapPosition) ? -1 : ((a.mapPosition < b.mapPosition) ? 1 : 0))
   const playersTableRows = (clan, opponent) => {
