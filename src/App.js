6f2fcdb76712362ad6cd9f2147d000c4fd11e411
Ion <ion.utale@icloud.com>
2019-12-03 16:25:53 +0100

    implemented the sidebar, router switch and haburger menu

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import CWL from './containers/CWL/CWL'
 import CWL_war from './containers/CWL_war/CWL_war'
-import packageJson from '../package.json';
+import packageJson from '../package.json'
+import Sidebar from './navigation/sidebar/sidebar'
+import {Switch, Route } from 'react-router-dom'
+import HamburgerMenu from 'react-hamburger-menu'
+
 
 function App() {
+  const [open, setOpen] = useState(true); 
+
+
   return (
     <div className="App">
       <header className="App-header">
-        Clan War League
-        <p className="Version">
-        Made With ❤️ By (_d0do_)  ©v{packageJson.version} 
+      <div className="Menu">
+        <HamburgerMenu
+          isOpen={open}
+          menuClicked={() => setOpen(!open) }
+          width={28}
+          height={25}
+          strokeWidth={1}
+          rotate={0}
+          color='white'
+          borderRadius={0}
+          animationDuration={0.2}
+        />
+      </div>
+          
+        <div className='Title'>
+          Clan War League
+          <p className="Version">
+            Made With <span  role="img" aria-label="love">❤️</span> By (_d0do_)  ©v{packageJson.version} 
           </p>
+        </div>
       </header>
-      {/* <CWL_war></CWL_war> */}
-      <CWL></CWL>
+      <Sidebar open={open}></Sidebar>
+
+      <Switch>
+        <Route  path="/" component={CWL}></Route>
+        <Route  path="/cwl/war/:tag" component={CWL_war}></Route>
+      </Switch>
+
     </div>
   );
 }
