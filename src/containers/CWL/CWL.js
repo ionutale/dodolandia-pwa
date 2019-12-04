2dc7aa8de4442a702a45ab6db584eef5f28b0104
Ion Utale <utale.ion@gmail.com>
2019-12-04 07:57:52 +0100

    minor fixs

--- src/containers/CWL/CWL.js
+++ src/containers/CWL/CWL.js
@@ -27,9 +27,7 @@ const CWL = (cwl) => {
     return ' ' 
   }
 
-  const largerMemberList = clans => {    
-    console.log(clans);
-    
+  const largerMemberList = clans => {        
     return clans.sort((a, b) => (a.members.length > b.members.length) ? -1 : ((a.members.length < b.members.length) ? 1 : 0))[0].members.length
   }
 
@@ -40,33 +38,33 @@ const CWL = (cwl) => {
 
     let rows = []
     rows.push(
-      <tr>
-        <th>#</th>
-        <th><img src={clans[0].badgeUrls.small} alt="clanBadge" /><br></br> {clans[0].name} <br></br> Membri:{clans[0].members.length}</th>
-        <th><img src={clans[1].badgeUrls.small} alt="clanBadge" /><br></br> {clans[1].name} <br></br> Membri:{clans[1].members.length}</th>
-        <th><img src={clans[2].badgeUrls.small} alt="clanBadge" /><br></br> {clans[2].name} <br></br> Membri:{clans[2].members.length}</th>
-        <th><img src={clans[3].badgeUrls.small} alt="clanBadge" /><br></br> {clans[3].name} <br></br> Membri:{clans[3].members.length}</th>
-        <th><img src={clans[4].badgeUrls.small} alt="clanBadge" /><br></br> {clans[4].name} <br></br> Membri:{clans[4].members.length}</th>
-        <th><img src={clans[5].badgeUrls.small} alt="clanBadge" /><br></br> {clans[5].name} <br></br> Membri:{clans[5].members.length}</th>
-        <th><img src={clans[6].badgeUrls.small} alt="clanBadge" /><br></br> {clans[6].name} <br></br> Membri:{clans[6].members.length}</th>
-        <th><img src={clans[7].badgeUrls.small} alt="clanBadge" /><br></br> {clans[7].name} <br></br> Membri:{clans[7].members.length}</th>
-      </tr>
+        <tr key='head-key'>
+          <th>#</th>
+          <th><img src={clans[0].badgeUrls.small} alt="clanBadge" /><br></br> {clans[0].name} <br></br> Membri:{clans[0].members.length}</th>
+          <th><img src={clans[1].badgeUrls.small} alt="clanBadge" /><br></br> {clans[1].name} <br></br> Membri:{clans[1].members.length}</th>
+          <th><img src={clans[2].badgeUrls.small} alt="clanBadge" /><br></br> {clans[2].name} <br></br> Membri:{clans[2].members.length}</th>
+          <th><img src={clans[3].badgeUrls.small} alt="clanBadge" /><br></br> {clans[3].name} <br></br> Membri:{clans[3].members.length}</th>
+          <th><img src={clans[4].badgeUrls.small} alt="clanBadge" /><br></br> {clans[4].name} <br></br> Membri:{clans[4].members.length}</th>
+          <th><img src={clans[5].badgeUrls.small} alt="clanBadge" /><br></br> {clans[5].name} <br></br> Membri:{clans[5].members.length}</th>
+          <th><img src={clans[6].badgeUrls.small} alt="clanBadge" /><br></br> {clans[6].name} <br></br> Membri:{clans[6].members.length}</th>
+          <th><img src={clans[7].badgeUrls.small} alt="clanBadge" /><br></br> {clans[7].name} <br></br> Membri:{clans[7].members.length}</th>
+        </tr>
     )
 
     for (let i = 0; i < largestClan; i++) {
 
       rows.push(
-        <tr>
-          <td>{i+1}</td>
-          <td>{hasObjectAtIndex(clans[0].members, i)}</td>
-          <td>{hasObjectAtIndex(clans[1].members, i)}</td>
-          <td>{hasObjectAtIndex(clans[2].members, i)}</td>
-          <td>{hasObjectAtIndex(clans[3].members, i)}</td>
-          <td>{hasObjectAtIndex(clans[4].members, i)}</td>
-          <td>{hasObjectAtIndex(clans[5].members, i)}</td>
-          <td>{hasObjectAtIndex(clans[6].members, i)}</td>
-          <td>{hasObjectAtIndex(clans[7].members, i)}</td>
-        </tr>
+          <tr key={i}>
+            <td>{i+1}</td>
+            <td>{hasObjectAtIndex(clans[0].members, i)}</td>
+            <td>{hasObjectAtIndex(clans[1].members, i)}</td>
+            <td>{hasObjectAtIndex(clans[2].members, i)}</td>
+            <td>{hasObjectAtIndex(clans[3].members, i)}</td>
+            <td>{hasObjectAtIndex(clans[4].members, i)}</td>
+            <td>{hasObjectAtIndex(clans[5].members, i)}</td>
+            <td>{hasObjectAtIndex(clans[6].members, i)}</td>
+            <td>{hasObjectAtIndex(clans[7].members, i)}</td>
+          </tr>
       )
     }
     return rows
@@ -76,7 +74,9 @@ const CWL = (cwl) => {
   return (
     <div>
       <table className={css.CompareTable}>
-        {tableRows(clans)}
+        <tbody>
+          {tableRows(clans)}
+        </tbody>
       </table>
     </div>
   );
